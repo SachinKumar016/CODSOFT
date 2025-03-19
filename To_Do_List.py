f2372eb6fffe14136cc220e9c2e14ab5d9469a1f
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-03-19 23:51:49 +0530

    5 commit

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,5 +1,9 @@
-import tkinter
-w=tkinter.Tk()
+from tkinter import *
+w=Tk()
 w.title("omala")
 w.geometry("500x500")
+input_box = Frame(w, bd=5, relief="ridge", width=500, height=100, bg='white', border=3)
+a=Label(input_box, text="saachin")
+input_box.pack()
+a.pack()
 w.mainloop()
